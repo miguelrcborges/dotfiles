c8ba6cfce724947c4b72ee8349bedbf099b7d009
Miguel Borges <mrcb08022002@gmail.com>
2022-06-04 23:44:39 +0100

    Windows: nvim plugins wouldn't be updated

--- update_config.bat
+++ update_config.bat
@@ -1,4 +1,4 @@
 @echo off
 del /q /s /f %USERPROFILE%\AppData\Local\nvim
-copy /y dots\.config\nvim %USERPROFILE%\AppData\Local\nvim
+xcopy /y /e /s dots\.config\nvim %USERPROFILE%\AppData\Local\nvim
 exit
