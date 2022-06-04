d78929308ad3767583771de41a2797438d42fe29
Miguel Borges <mrcb08022002@gmail.com>
2022-06-04 13:25:36 +0100

    nvim very pog now

--- dots/.config/nvim/plugin/lsp.lua
+++ dots/.config/nvim/plugin/lsp.lua
@@ -2,5 +2,25 @@ lsp = require("lspconfig")
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lsp.clangd.setup{ capabilities = capabilities }
-lsp.gopls.setup{ capabilities = capabilities }
+
+local servers = { 'gopls', 'clangd' }
+
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, bufopts)
+
+end
+
+
+for _, lsp in pairs(servers) do
+  require('lspconfig')[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
