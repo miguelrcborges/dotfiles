8b4d60a1b2e1b06fc6d8e2eb2a61fdd3c38d3932
Miguel Borges <mrcb08022002@gmail.com>
2022-06-09 01:13:02 +0100

    fix windows not having bashrc

--- update_config.bat
+++ update_config.bat
@@ -5,7 +5,7 @@ del /q /s /f %USERPROFILE%\AppData\Local\nvim
 xcopy /y /e /s dots\.config\nvim %USERPROFILE%\AppData\Local\nvim
 
 :: Bash
-del /q /s /f %USERPROFILE%\.bashrc
-xcopy /y /e /s dots\.bashrc %USERPROFILE\.bashrc
+del /q %USERPROFILE%\.bashrc
+xcopy /y /q dots\.bashrc %USERPROFILE%\.bashrc
 
 exit
