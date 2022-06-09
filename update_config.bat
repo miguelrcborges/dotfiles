12151737f3d383dad80ca2fa3e9ef80a9397626f
Miguel Borges <mrcb08022002@gmail.com>
2022-06-09 01:49:38 +0100

    add .bashrc to windows script

--- update_config.bat
+++ update_config.bat
@@ -1,4 +1,11 @@
 @echo off
+
+:: Nvim
 del /q /s /f %USERPROFILE%\AppData\Local\nvim
 xcopy /y /e /s dots\.config\nvim %USERPROFILE%\AppData\Local\nvim
+
+:: Bash
+del /q /s /f %USERPROFILE%\.bashrc
+xcopy /y /e /s dots\.bashrc %USERPROFILE\.bashrc
+
 exit
