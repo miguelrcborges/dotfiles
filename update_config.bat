3d5edb161cbd8e7d2b472594e3349b2af991cc5a
Miguel Borges <mrcb08022002@gmail.com>
2022-06-18 03:39:52 +0100

    fix windows names that have spaces

--- update_config.bat
+++ update_config.bat
@@ -1,11 +1,11 @@
 @echo off
 
 :: Nvim
-del /q /s /f %USERPROFILE%\AppData\Local\nvim
-xcopy /y /e /s dots\.config\nvim %USERPROFILE%\AppData\Local\nvim
+del /q /s /f "%USERPROFILE%\AppData\Local\nvim"
+xcopy /y /e /s "dots\.config\nvim" "%USERPROFILE%\AppData\Local\nvim"
 
 :: Bash
-del /q %USERPROFILE%\.bashrc
-xcopy /y /q dots\.bashrc %USERPROFILE%\.bashrc
+del /q "%USERPROFILE%\.bashrc"
+xcopy /y /q "dots\.bashrc" "%USERPROFILE%\.bashrc"
 
 exit
