804cd5419630deb6b0f75dae14b161e589249a1f
Miguel Borges <mrcb08022002@gmail.com>
2022-06-24 14:55:50 +0100

    make code formatting possible + changing my hover keybinds

--- dots/.config/nvim/plugin/lsp.lua
+++ dots/.config/nvim/plugin/lsp.lua
@@ -8,12 +8,12 @@ local servers = { 'gopls', 'clangd' }
 local on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<leader>fm', vim.lsp.buf.formatting, bufopts)
   vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, bufopts)
 
 end
 
