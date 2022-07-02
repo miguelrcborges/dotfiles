a37c871385bf2de5381b65f4aa38699c8449c448
Miguel Borges <mrcb08022002@gmail.com>
2022-07-02 21:03:47 +0100

    updoot

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -S bspwm sxhkd xorg xorg-xinit stow rofi alacritty feh wireplumber pipewire pipewire-alsa pipewire-jack pipewire-pulse pulsemixer maim xclip
+sudo pacman -S bspwm sxhkd xorg xorg-xinit stow rofi alacritty feh wireplumber pipewire pipewire-alsa pipewire-jack pipewire-pulse pulsemixer maim xclip noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
 
 systemctl enable --user pipewire --now
 systemctl enable --user pipewire-pulse --now
@@ -13,3 +13,30 @@ chmod +x .config/bspwm/bspwmrc
 yay -S nerd-fonts-jetbrains-mono librewolf catppuccin-gtk-theme
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo echo '<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+  <match target="font">
+    <edit name="antialias" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit name="hinting" mode="assign">
+      <bool>true</bool>
+    </edit>
+    <edit mode="assign" name="rgba">
+      <const>rgb</const>
+    </edit>
+    <edit mode="assign" name="hintstyle">
+      <const>hintslight</const>
+    </edit>
+    <edit mode="assign" name="lcdfilter">
+      <const>lcddefault</const>
+    </edit>
+  </match>
+</fontconfig>' >> /etc/fonts/local.conf
+
+sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+
