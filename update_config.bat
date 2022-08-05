41d3b5d36a8ef5525c639956a55c4dfcff2a9b0b
Miguel Borges <mrcb08022002@gmail.com>
2022-08-05 02:19:03 +0100

    mpv config

--- update_config.bat
+++ update_config.bat
@@ -8,4 +8,12 @@ xcopy /y /e /s "dots\.config\nvim" "%USERPROFILE%\AppData\Local\nvim"
 del /q "%USERPROFILE%\.bashrc"
 xcopy /y /q "dots\.bashrc" "%USERPROFILE%\.bashrc"
 
+:: MPV
+
+del /q "%USERPROFILE%\Documents\mpv\shaders"
+del /q "%USERPROFILE%\Documents\mpv\input.conf"
+del /q "%USERPROFILE%\Documents\mpv\mpv.conf"
+xcopy /y /e /s "dots\.config\mpv\shaders" "%USERPROFILE%\Documents\mpv\shaders"
+xcopy /y /q "dots\.config\mpv\input.conf" "%USERPROFILE%\Documents\mpv\input.conf"
+xcopy /y /q "dots\.config\mpv\mpv.conf" "%USERPROFILE%\Documents\mpv\mpv.conf"
 exit
