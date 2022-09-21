048717d1193bc0ba9beb628a0115d1759b9a137e
Miguel Borges <mrcb08022002@gmail.com>
2022-09-22 00:55:48 +0100

    oopsie I still dont know why { doesnt autopair

--- dots/.config/nvim/plugin/completion.lua
+++ dots/.config/nvim/plugin/completion.lua
@@ -45,3 +45,10 @@ cmp.setup({
     { name = 'buffer' },
   })
 })
+
+-- extra 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
